e43129b3f1b677cd6116eb39aa8b2f8e617567fe
MadDevX <eryk.dobrosz@gmail.com>
2019-10-20 13:19:21 +0200

    Add weapon system architecture stubs

---
+++ Assets/Scripts/WeaponSystem/Projectiles/Physics/ProjectilePhysics.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectilePhysics// : MonoFixedUpdatableObject moze pocisk to klasa c#? a nie obiekt unity?
+{
+}
---
+++ Assets/Scripts/WeaponSystem/Weapons/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : IWeapon
+{
+
+
+    public void Reload()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Shoot()
+    {
+        throw new System.NotImplementedException();
+    }
+}
