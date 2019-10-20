b053a0402cba6db4c2482aa01f68912234f326c7
MadDevX <eryk.dobrosz@gmail.com>
2019-10-20 14:55:53 +0200

    Import DarkRiftNetworking, add automatic dll imports for standalone and Unity servers

---
+++ IntoTheHivePlugins/ExamplePlugin.cs
@@ -0,0 +1,17 @@
+﻿using DarkRift.Server;
+using System;
+
+namespace ServerPlugins
+{
+    public class ExamplePlugin : Plugin
+    {
+        public override bool ThreadSafe => false;
+
+        public override Version Version => new Version(1, 0, 0);
+
+        public ExamplePlugin(PluginLoadData pluginLoadData) : base(pluginLoadData)
+        {
+        }
+
+    }
+}
---
+++ IntoTheHivePlugins/UpdatePluginDLL.bat
@@ -0,0 +1,4 @@
+@echo off
+xcopy /Y/Q %1 %2
+xcopy /Y/Q %1 %3
+echo Copied %1 into %2 and %3 directories.
