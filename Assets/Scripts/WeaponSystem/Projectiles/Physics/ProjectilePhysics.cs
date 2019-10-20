b503bc80a2409e18424b1cbcf1337bb2f22206f6
MadDevX <eryk.dobrosz@gmail.com>
2019-10-20 20:50:46 +0200

    Add future implementation hints for projectile physics

--- Assets/Scripts/WeaponSystem/Projectiles/Physics/ProjectilePhysics.cs
+++ Assets/Scripts/WeaponSystem/Projectiles/Physics/ProjectilePhysics.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectilePhysics// : MonoFixedUpdatableObject moze pocisk to klasa c#? a nie obiekt unity?
+//C# class - requires GameObjectContext (hell no) and appropriate installer 
+//thus - if projectile will have context either way, then Physics can be pure C#, if Context will not be required - make it MonoBehaviour
+//MonoUpdatables will come in handy either way, so why not both
+public class ProjectilePhysics// : MonoFixedUpdatableObject 
 {
 }
