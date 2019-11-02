e8212aeea86cc57df3bc05045ee67301104be5e2
Marcin Gackowski <marcinga4@hotmail.com>
2019-11-02 15:30:55 +0100

    Create draft Player Spawner and poolable characterFacade

---
+++ Assets/Scripts/Character/CharacterFacade.cs
@@ -0,0 +1,58 @@
+﻿using GameLoop;
+using System;
+using UnityEngine;
+using Zenject;
+
+public struct CharacterSpawnParameters
+{
+    // Placeholder
+    public ushort clientId;
+    public Vector2 position;
+    public IWeapon weapon;
+
+
+    public CharacterSpawnParameters(
+        ushort clientId,
+        IWeapon weapon,
+        Vector2 position)
+    {
+        this.clientId = clientId;
+        this.weapon = weapon;
+        this.position = position;
+    }
+}
+
+public class CharacterFacade: MonoUpdatableObject, IPoolable<CharacterSpawnParameters, IMemoryPool>, IDisposable
+{ 
+    private IMemoryPool _pool;
+
+    public CharacterFacade()
+    {
+        
+    }
+
+    public void Dispose()
+    {
+        _pool.Despawn(this);
+    }
+
+    public void OnDespawned()
+    {
+        _pool = null;
+    }
+
+    public void OnSpawned(CharacterSpawnParameters parameters, IMemoryPool pool)
+    {
+       
+        _pool = pool;
+
+    }
+
+    public override void OnUpdate(float deltaTime)
+    {
+    }
+
+    public class Factory: PlaceholderFactory<CharacterSpawnParameters, CharacterFacade>
+    {
+    }
+}
--- Assets/Scripts/Installers/GameplayInstaller.cs
+++ Assets/Scripts/Installers/GameplayInstaller.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using DarkRift.Client.Unity;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
@@ -6,9 +7,11 @@ using Zenject;
 public class GameplayInstaller : MonoInstaller
 {
     [SerializeField] private Camera _mainCamera;
+    [SerializeField] private UnityClient _client;
 
     public override void InstallBindings()
     {
         Container.Bind<Camera>().FromInstance(_mainCamera).AsSingle();
+        Container.Bind<UnityClient>().FromInstance(_client).AsSingle();
     }
 }
