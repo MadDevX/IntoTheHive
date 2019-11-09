14c35874344989dc9c7cdbfc1c55deae0d07a4d4
Marcin Gackowski <marcinga4@hotmail.com>
2019-11-09 16:26:39 +0100

    Remove invalid references

--- Assets/Scripts/Network/Installers/NetworkedReceiverInstaller.cs
+++ Assets/Scripts/Network/Installers/NetworkedReceiverInstaller.cs
@@ -4,8 +4,6 @@ public class NetworkedReceiverInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
-        Container.BindInterfacesAndSelfTo<InputMessageReceiver>().AsSingle();
-        Container.BindInterfacesAndSelfTo<EquipmentMessageReceiver>().AsSingle();
         Container.BindInterfacesAndSelfTo<NetworkedCharacterInput>().AsSingle();
         Container.BindInterfacesAndSelfTo<NetworkedCharacterEquipment>().AsSingle();
     }
