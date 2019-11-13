d317f3a24ef2547b9938b8c870c93aa6239f33e8
Marcin Gackowski <marcinga4@hotmail.com>
2019-11-13 19:51:51 +0100

    Add Client Info which differentiates host from client
    ClientInfo stores information whether an application is functioning as host or a client

---
+++ Assets/Scripts/Network/ClientInfo.cs
@@ -0,0 +1,14 @@
+﻿using DarkRift.Client.Unity;
+
+public class ClientInfo
+{
+    // TODO MG: UnityClient is probably unnecessary here
+    public UnityClient Client;
+    public ClientStatus Status = ClientStatus.None;
+
+    public ClientInfo(
+        UnityClient client)
+    {
+        Client = client;
+    }
+}
