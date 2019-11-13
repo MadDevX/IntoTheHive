3ca8602a22c95fc2d9bac9dba49d81e14a0a5c6d
Marcin Gackowski <marcinga4@hotmail.com>
2019-11-13 17:35:40 +0100

    Add scene changed message sender and fix CreateServer button
    Create server button is no longer dependant on input fields and change scene messages are sent by ChangeSceneMessageSender

---
+++ Assets/Scripts/Network/MessageSenders/ChangeSceneMessageSender.cs
@@ -0,0 +1,52 @@
+﻿using DarkRift;
+using DarkRift.Client.Unity;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneMessageSender
+{
+    private UnityClient _client;
+
+    public ChangeSceneMessageSender(
+        UnityClient client)
+    {
+        _client = client;
+    }
+
+    public void SendSceneChanged(ushort sceneBuildIndex)
+    {
+        using (DarkRiftWriter writer = DarkRiftWriter.Create())
+        {
+            writer.Write(sceneBuildIndex);
+
+            using (Message message = Message.Create(Tags.ChangeScene, writer))
+            {
+                _client.SendMessage(message,SendMode.Reliable);
+            }
+        }
+    }
+
+    public void SendSceneChanged(string sceneName)
+    {
+        var scene = SceneManager.GetSceneByName(sceneName);
+        SendSceneChanged((ushort)scene.buildIndex);
+    }
+
+    public void SendSceneChangedClientsOnly(ushort sceneBuildIndex)
+    {
+        using (DarkRiftWriter writer = DarkRiftWriter.Create())
+        {
+            writer.Write(sceneBuildIndex);
+
+            using (Message message = Message.Create(Tags.ChangeSceneClientsOnly, writer))
+            {
+                _client.SendMessage(message, SendMode.Reliable);
+            }
+        }
+    }
+
+    public void SendSceneChangedClientsOnly(string sceneName)
+    {
+        var scene = SceneManager.GetSceneByName(sceneName);
+        SendSceneChangedClientsOnly((ushort)scene.buildIndex);
+    }
+}
