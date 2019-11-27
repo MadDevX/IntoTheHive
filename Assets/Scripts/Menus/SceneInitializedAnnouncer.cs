a0ef6d97097e84aadfefed8700d9f05aaded0de5
Marcin Gackowski <marcinga4@hotmail.com>
2019-11-27 21:47:40 +0100

    Move SceneInitializedAnnouncer class to another folder

---
+++ Assets/Scripts/Menus/SceneInitializedAnnouncer.cs
@@ -0,0 +1,19 @@
+﻿using GameLoop;
+using System;
+using UnityEngine;
+
+public class SceneInitializedAnnouncer: MonoUpdatableObject
+{
+    public event Action SceneInitialized;
+
+    private void Start()
+    {
+        SceneInitialized?.Invoke();
+        Debug.Log("Scene Initialized");
+    }
+
+    public override void OnUpdate(float deltaTime)
+    {
+        UnsubscribeLoop();
+    }
+}
