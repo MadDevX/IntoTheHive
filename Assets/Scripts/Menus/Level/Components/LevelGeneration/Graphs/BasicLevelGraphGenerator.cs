612fca5779b3ac5c95713db670856238a13a93ef
Marcin Gackowski <marcinga4@hotmail.com>
2019-11-28 22:04:19 +0100

    Add LevelGraph class and LevelGenerator draft
    Add a levelgraph which will represent room layout and add a simple draft of levelGenerator class which will create levels based on the information in the graph.
    Spawning from prefab and networking is yet to be done.

---
+++ Assets/Scripts/Menus/Level/Components/LevelGeneration/Graphs/BasicLevelGraphGenerator.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// This class generates basic level graphs
+/// </summary>
+public class BasicLevelGraphGenerator : IGraphGenerable
+{
+    // change this to BasicLevelGraphFactory?
+
+    public LevelGraph GenerateLevelGraph()
+    {
+        LevelGraph graph = new LevelGraph();
+
+        graph.AddVertex(0);
+        graph.AddVertex(0);
+        graph.AddVertex(0);
+        graph.AddVertex(0);
+        graph.AddVertex(0);
+        graph.AddVertex(0);
+
+        graph.AddEdge(0, 1, GraphDirection.North);
+        graph.AddEdge(1, 2, GraphDirection.West);
+        graph.AddEdge(2, 3, GraphDirection.North);
+        graph.AddEdge(1, 4, GraphDirection.East);
+        graph.AddEdge(4, 5, GraphDirection.North);
+
+        return graph;
+    }
+}
---
+++ Assets/Scripts/Menus/Level/Components/LevelGeneration/Graphs/LevelGraph.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+/// <summary>
+/// Class representing levels as planar graphs with four children, each representing one direction
+/// </summary>
+public class LevelGraph
+{
+    public List<LevelGraphVertex> nodes;
+
+    public LevelGraph()
+    {
+        nodes = new List<LevelGraphVertex>();
+    }
+
+    public int AddVertex(ushort roomId)
+    {
+        var vertex = new LevelGraphVertex(roomId);
+        nodes.Add(vertex);
+        return nodes.IndexOf(vertex);
+    }
+
+    public void RemoveVertex(int Id)
+    {
+        nodes.RemoveAt(Id);
+    }
+
+    public void AddEdge(int fromId, int toId, GraphDirection direction)
+    {
+        var from = nodes[fromId];
+        var to = nodes[toId];
+        from.AddNeighbour(toId, direction);
+        GraphDirection reversedDirection = (GraphDirection) (3 - (int)direction);
+        to.AddNeighbour(fromId, reversedDirection);
+    }
+}
+
+
