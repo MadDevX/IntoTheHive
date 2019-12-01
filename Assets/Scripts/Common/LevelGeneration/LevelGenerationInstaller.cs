84900c91b7c4acd9dcf252212efbd5e199f76e64
Marcin Gackowski <marcinga4@hotmail.com>
2019-12-01 16:45:52 +0100

    Add installer

---
+++ Assets/Scripts/Common/LevelGeneration/LevelGenerationInstaller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Zenject;
+
+public class LevelGenerationInstaller: MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        BindComponents();
+        BindFactory();
+        BindMessaging();        
+    }
+
+    private void BindComponents()
+    {
+        Container.Bind<Rooms>().AsSingle();
+        Container.BindInterfacesAndSelfTo<LevelSpawner>().AsSingle();
+        Container.BindInterfacesAndSelfTo<LevelGraphState>().AsSingle();
+        Container.BindInterfacesAndSelfTo<BasicLevelGraphGenerator>().AsSingle();
+        Container.BindInterfacesAndSelfTo<SpawnParametersGenerator>().AsSingle();
+    }
+
+    private void BindFactory()
+    {
+        Container.BindFactory<RoomSpawnParameters, RoomFacade, RoomFacade.Factory>().FromFactory<RoomFactory>();
+    }
+
+    private void BindMessaging()
+    {
+        Container.BindInterfacesAndSelfTo<LevelGraphMessageReceiver>().AsSingle();
+        Container.BindInterfacesAndSelfTo<LevelGraphMessageSender>().AsSingle();
+    }
+}
