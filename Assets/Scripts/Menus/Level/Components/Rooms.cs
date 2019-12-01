2d5c27ba1a5bb556de7ea6c631dfe333b8b0e632
Marcin Gackowski <marcinga4@hotmail.com>
2019-12-01 16:45:44 +0100

    Add doors in graph translator and rename it. Add Rooms prefabs.
    LevelGraphTranslator is now SpawnParametersGenerator.

--- Assets/Scripts/Menus/Level/Components/Rooms.cs
+++ Assets/Scripts/Menus/Level/Components/Rooms.cs
@@ -1,8 +1,17 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 /// <summary>
 /// This class holds all rooms' prefabs
 /// </summary>
 public class Rooms
 {
-  
+    [SerializeField] public Object BasicRoom;
+    [SerializeField] public Object Door;
+
+    public Object GetRoomById(ushort roomId)
+    {
+        if (roomId == 0) return BasicRoom;
+        if (roomId == 1) return Door;
+        else return BasicRoom;
+    }
 }
