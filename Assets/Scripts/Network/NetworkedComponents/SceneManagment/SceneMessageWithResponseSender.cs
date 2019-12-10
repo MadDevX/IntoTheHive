eb929b98d3c67caf6f149d4044c76fe713d79584
Marcin Gackowski <marcinga4@hotmail.com>
2019-12-10 20:58:46 +0100

    Generic messages with response and project events
    Generic messages with response are now fully implemented.
    
    Project Events have been introduced to artificially schedule execution of methods in another scene without having a bound instance to refer to. For Example lobby calls scene change but level generation in scene context based so it is executed when the event is invoked.

--- Assets/Scripts/Network/NetworkedComponents/SceneManagment/SceneMessageWithResponseSender.cs
+++ Assets/Scripts/Network/NetworkedComponents/SceneManagment/SceneMessageWithResponseSender.cs
@@ -1,159 +1,57 @@
 ﻿using DarkRift;
-using DarkRift.Client.Unity;
 using System;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 using Zenject;
-
-// TODO MG : Future idea - generalize class to provide a mechanism for any message type
 
 /// <summary>
 /// This class sends a SceneChanged message to all clients and executes a given action when all of the clients respond with a SceneReady message.
 /// </summary>
-public class SceneMessageWithResponse : IInitializable, IDisposable
+public class SynchronizedSceneManager : IInitializable, IDisposable
 {
-    public event Action Completed;
-
     private NetworkRelay _relay;
-    private UnityClient _client;
-    private SceneMessageSender _messageSender;
-    private GlobalHostPlayerManager _playerManager;
     private ScenePostinitializationEvents _postInitEvents;
+    private GenericMessageWithResponseHost _messageWithResponseHost;
+    private GenericMessageWithResponseClient _messageWithResponseClient;
 
-    private Dictionary<ushort, bool> PlayersWithSceneReady;
-
-    public SceneMessageWithResponse(
+    public SynchronizedSceneManager(
         NetworkRelay relay,
-        UnityClient client,
-        SceneMessageSender sender,
-        GlobalHostPlayerManager playerManager,
-        ScenePostinitializationEvents postInitEvents)
+        ScenePostinitializationEvents postInitEvents,
+        GenericMessageWithResponseHost messageWithResponseHost,
+        GenericMessageWithResponseClient messageWithResponseClient)
     {
-        PlayersWithSceneReady = new Dictionary<ushort, bool>();
         _relay = relay;
-        _client = client;
-        _messageSender = sender;
-        _playerManager = playerManager;
         _postInitEvents = postInitEvents;
+        _messageWithResponseHost = messageWithResponseHost;
+        _messageWithResponseClient = messageWithResponseClient;
     }
 
     public void Initialize()
     {
-        _relay.Subscribe(Tags.SceneReady, HandleSceneReady);
-        _relay.Subscribe(Tags.ChangeSceneWithReply, ParseChangeSceneWithResponseMessage);
+        _relay.Subscribe(Tags.ChangeSceneWithReply, ParseMessage);
     }
 
     public void Dispose()
     {
-        _relay.Unsubscribe(Tags.SceneReady, HandleSceneReady);
-        _relay.Unsubscribe(Tags.ChangeSceneWithReply, ParseChangeSceneWithResponseMessage);
+        _relay.Unsubscribe(Tags.ChangeSceneWithReply, ParseMessage);
     }
     
-    /// <summary>
-    /// Send SceneChanged message to all clients and invoke an action after all recepients loaded their scenes and responded.
-    /// Each call of this method clears previously subscribed events and requires no additional operations to work.
-    /// </summary>
-    /// <param name="buildIndex">BuildIndex of the scene to be loaded</param>
-    /// <param name="actionOnComplete">Action to be invoked when all clients load the scene and respond</param>
-    public void SendSceneChangedWithResponse(int buildIndex, Action actionOnComplete)
+    public void SendSceneChanged(int buildIndex, Action onComplete)
     {
-        ResetDictionary();
-        this.Completed += actionOnComplete;
+        _messageWithResponseHost.SendMessageWithResponse(PrepareMessage(buildIndex), onComplete);
+    }
 
+    public Message PrepareMessage(int buildIndex)
+    {
         using (DarkRiftWriter writer = DarkRiftWriter.Create())
         {
             ushort buildIndexUint = (ushort)buildIndex;
             writer.Write(buildIndexUint);
 
-            using (Message message = Message.Create(Tags.ChangeSceneWithReply, writer))
-            {
-                _client.SendMessage(message, SendMode.Reliable);
-            }
+            return Message.Create(Tags.ChangeSceneWithReply, writer);
         }
-    }
+    }   
 
-    /// <summary>
-    /// Sends a reply notifying that a scene is ready. Most probably located in sceneInitializedHandler
-    /// </summary>
-    public void SceneReady()
-    {
-        _messageSender.SendSceneReady();
-    }
-
-    /// <summary>
-    /// Resets the current dictionary with information who has a scene ready and who doesn't.
-    /// New Dictionary is filled based on the ConnectedPlayers list from GlobalHostPlayerManager
-    /// </summary>
-    private void ResetDictionary()
-    {
-        PlayersWithSceneReady.Clear();
-        
-        foreach(ushort client in _playerManager.ConnectedPlayers)
-        {
-            Debug.Log("added client " + client + " to the dictionary");
-            PlayersWithSceneReady.Add(client, false);
-        }
-    }
-
-    /// <summary>
-    /// Handles a response from a client who received the original message.
-    /// </summary>
-    /// <param name="message">Reponse from the client.</param>
-    private void HandleSceneReady(Message message)
-    {
-        using (DarkRiftReader reader = message.GetReader())
-        {
-            //TODO MG CHECKSIZE
-            ushort id = reader.ReadUInt16();
-            Debug.Log("id of joining player" + id);
-            if(PlayersWithSceneReady.ContainsKey(id))
-            {
-                PlayersWithSceneReady.Remove(id);
-            }
-            else
-            {
-                throw new ArgumentException("Received a message from client not present in the game");
-            }
-            this.PlayersWithSceneReady.Add(id, true);
-            AreAllScenesReady();
-        }
-    }
-
-    /// <summary>
-    /// Checks if all players sent a SceneReady message. 
-    /// If that is the case, the action is invoked and all subscribed actions cleared.
-    /// </summary>
-    private void AreAllScenesReady()
-    {
-        bool allReady = true;
-
-        foreach (ushort client in PlayersWithSceneReady.Keys)
-        {
-            allReady = allReady && PlayersWithSceneReady[client];
-        }
-
-        if(allReady && PlayersWithSceneReady.Keys.Count > 0)
-        {
-            Completed?.Invoke();
-            ClearSubscribedActions();
-        }
-
-    }
-
-    private void ClearSubscribedActions()
-    {
-        foreach (var action in Completed.GetInvocationList())
-        {
-            Completed -= (Action)action;
-        }
-    }
-
-    /// <summary>
-    /// Parses the original message sent by host.
-    /// </summary>
-    /// <param name="message">A ChangeSceneWithResponse message</param>
-    private void ParseChangeSceneWithResponseMessage(Message message)
+    private void ParseMessage(Message message)
     {
         int sceneBuildIndex;
 
@@ -162,10 +60,10 @@ public class SceneMessageWithResponse : IInitializable, IDisposable
             sceneBuildIndex = reader.ReadUInt16();
         }
 
+        // Loads a sceme and Sends ClientReady when scene is loaded and properly initialized
         SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
-        // Sends SceneReady when scene is loaded and properly initialized
-        // TODO MG : REMEMBER TO UNSUBSCRIBE
-        _postInitEvents.Subscribe(sceneBuildIndex, SceneReady);
+        _postInitEvents.Subscribe(sceneBuildIndex, _messageWithResponseClient.SendClientReady);
+
     }
 }
 
