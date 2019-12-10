f8c0347d043d777a12f61331564622d1e8293689
Marcin Gackowski <marcinga4@hotmail.com>
2019-12-10 21:45:19 +0100

    Add HostSceneManager which takes care of firing correct events
    HostSceneManager fires correct events after completing synchronized messages

---
+++ Assets/Scripts/Network/NetworkedComponents/SceneManagment/HostSceneManager.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// In this class synchronized scene changes are stores with their corresponding events so that other classes do not need to know which event to fire.
+/// </summary>
+public class HostSceneManager
+{
+    private SynchronizedSceneManager _synchronizedSceneManager;
+    private ProjectEventManager _eventManager;
+
+    public HostSceneManager(
+        SynchronizedSceneManager synchronizedSceneManager,
+        ProjectEventManager eventManager)
+    {
+        _eventManager = eventManager;
+        _synchronizedSceneManager = synchronizedSceneManager;
+    }
+
+    /// <summary>
+    /// Sends a synchronized message to all clients and fires GameInitializedHost which is used by GameplayInitializer.
+    /// </summary>
+    public void LoadNextLevel()
+    {
+        _synchronizedSceneManager.SendSceneChanged(3, _eventManager.FireGameInitializedHost);
+    }
+
+    public void LoadHub()
+    {
+
+    }
+
+    public void LoadMenu()
+    {
+
+    }
+
+}
+
