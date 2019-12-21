825877fed25085e4f84a7a0558fda22a2e8de84a
Marcin Gackowski <marcinga4@hotmail.com>
2019-12-21 23:28:50 +0100

    Add RoomTriggers that close doors
    When the room is entered by the whole team, the HostEncounterManager calls BeginEncounter and the doors close.

---
+++ Assets/Scripts/Encounter/HostEncounterManager.cs
@@ -0,0 +1,52 @@
+﻿/// <summary>
+/// Handles the flow of the encounter. Begins it reacting to the RoomEnteredTrigger and end when all enemies are dead.
+/// </summary>
+public class HostEncounterManager
+{
+    private GenericMessageWithResponseHost _messageWithResponse;
+    private HostDoorManager _doorManager;
+    // private HostEncouterEnemyManager - spawns enemies and invokes AllwavesClearedEvent
+    // 
+
+    // TEMPORARY
+    private NetworkedAISpawner _aISpawner;
+    // TEMPORARY
+
+    private int _encounterID = -1;
+
+    public HostEncounterManager(
+        NetworkedAISpawner aISpawner,
+
+        GenericMessageWithResponseHost messageWithResponse,
+        HostDoorManager doorManager)
+    {
+        _aISpawner = aISpawner;
+
+        _messageWithResponse = messageWithResponse;
+        _doorManager = doorManager;
+    }
+
+    public void BeginEncounter(ushort doorId)
+    {
+        _encounterID = doorId;
+        // mark room as visited
+        // spawn doors 
+        SpawnDoors();
+    }
+
+    public void SpawnDoors()
+    {
+        _messageWithResponse.SendMessageWithResponse(_doorManager.PrepareCloseDoorsMessage((ushort)_encounterID), SpawnEnemies);
+    }
+
+    public void SpawnEnemies()
+    {
+        //_messageWithResponse.SendMessageWithResponse(_aISpawner.GenerateSpawnMessage(0, 10));
+    }
+
+    public void EndEncounter()
+    {
+        _messageWithResponse.SendMessageWithResponse(_doorManager.PrepareOpenDoorsMessage((ushort)_encounterID));
+    }
+
+}
---
+++ Assets/Scripts/Triggers/SpecificTriggerLogic/RoomEnteredTrigger/RoomEnteredTriggerInstaller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class RoomEnteredTriggerInstaller: MonoInstaller
+{
+    [SerializeField] public RoomFacade _roomFacade;
+
+    public override void InstallBindings()
+    {
+        Container.BindInterfacesAndSelfTo<RoomEnteredTrigger>().AsSingle();
+        Container.BindInstance(_roomFacade);
+    }
+}
