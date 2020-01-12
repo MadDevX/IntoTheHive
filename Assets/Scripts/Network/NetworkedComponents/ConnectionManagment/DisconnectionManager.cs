ad215551eb0b0471ddcb1d46d5de1312dfa7d04f
Marcin Gackowski <marcinga4@hotmail.com>
2020-01-12 23:48:43 +0100

    Reset data after disconnection from the game

--- Assets/Scripts/Network/NetworkedComponents/ConnectionManagment/DisconnectionManager.cs
+++ Assets/Scripts/Network/NetworkedComponents/ConnectionManagment/DisconnectionManager.cs
@@ -6,27 +6,30 @@ using Zenject;
 public class DisconnectionManager: IInitializable, IDisposable
 {
     private UnityClient _client;
-
+    private GameCycle _gameCycle;
     public DisconnectionManager(
-        UnityClient client)
+        UnityClient client,
+        GameCycle gameCycle)
     {
         _client = client;
+        _gameCycle = gameCycle;
     }
 
 
     public void Initialize()
     {
-        _client.Disconnected += HandleDisconnect;
+        _client.Disconnected += HandleDisconnected;
     }
 
     public void Dispose()
     {
-        _client.Disconnected -= HandleDisconnect;
+        _client.Disconnected -= HandleDisconnected;
     }
 
-    private void HandleDisconnect(object sender, DarkRift.Client.DisconnectedEventArgs e)
+    private void HandleDisconnected(object sender, DarkRift.Client.DisconnectedEventArgs e)
     {
         SceneManager.LoadScene(1, LoadSceneMode.Single);
+        _gameCycle.RaiseOnGameEnded();
     }
 }
 
