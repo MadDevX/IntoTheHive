9d2e4aea4dfa9cf3603e34a3690922945578ee3a
Marcin Gackowski <marcinga4@hotmail.com>
2020-01-12 23:39:27 +0100

    Add Quit button the the pause menu

---
+++ Assets/Scripts/Menus/PauseMenu/Components/ExitPauseMenuManager.cs
@@ -0,0 +1,47 @@
+﻿using DarkRift.Client.Unity;
+using System;
+using UnityEngine.UI;
+using Zenject;
+
+public class ExitPauseMenuManager: IInitializable, IDisposable
+{
+    private ServerManager _serverManager;
+    private UnityClient _client;
+    private ClientInfo _clientInfo;
+    private Button _exitButton;
+    public ExitPauseMenuManager(
+        [Inject(Id =Identifiers.PauseExitButton)]
+        Button exitButton,
+        ServerManager serverManager,
+        UnityClient client,
+        ClientInfo clientInfo)
+    {
+        _exitButton = exitButton;
+        _serverManager = serverManager;
+        _client = client;
+        _clientInfo = clientInfo;
+    }
+   
+
+    public void Initialize()
+    {
+        _exitButton.onClick.AddListener(QuitGame);
+    }
+
+    public void Dispose()
+    {
+        _exitButton.onClick.RemoveListener(QuitGame);
+    }
+
+    private void QuitGame()
+    {
+        if(_clientInfo.Status == ClientStatus.Host)
+        {
+            _serverManager.CloseServer();
+        }
+        if (_clientInfo.Status == ClientStatus.Client)
+        {
+            _client.Disconnect();
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/PauseMenu/Installers/ExitPauseMenuInstaller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class ExitPauseMenuInstaller : MonoInstaller
+{
+    [SerializeField] private Button _exitButton;    
+
+    public override void InstallBindings()
+    {
+        InstallLogic();
+        InstallElements();
+    }
+
+    public void InstallLogic()
+    {
+        Container.BindInterfacesAndSelfTo<ExitPauseMenuManager>().AsSingle();
+    }
+    public void InstallElements()
+    {
+        Container.BindInstance(_exitButton).WithId(Identifiers.PauseExitButton).AsCached();
+    }
+}
